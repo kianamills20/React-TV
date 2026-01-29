1896b2f19ee6f523f6e94b691d3f69412f180f1b
Kiana Mills <kiana.mills20@gmail.com>
2026-01-29 00:31:49 -0800

    trying to render

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+import { tvShow } from "./shows/data";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection"
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [shows] = useState(tvShow);
+  const [selectedShow, setSelectedShow] = useState();
   return (
     <>
       <header>
         <p>React TV</p>
+<ShowSelection 
+  shows={shows}
+  selectedShow={selectedShow}
+  setSelectedShow={setSelectedShow}
+/>
       </header>
-      <main></main>
+      <main>
+        <ShowDetails key={selectedShow?.name} shows={selectedShow} />
+      </main>
     </>
   );
 }
