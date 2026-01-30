06f08c20e148cc2b13f04835286e700d137f1202
Kiana Mills <kiana.mills20@gmail.com>
2026-01-30 10:00:27 -0800

    found why my show details was not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { useState } from "react";
-import { tvShow } from "./shows/data";
+import { tvShows } from "./shows/data";
 import ShowDetails from "./shows/ShowDetails";
-import ShowSelection from "./shows/ShowSelection"
+import ShowSelection from "./shows/ShowSelection";
 
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
-  const [shows] = useState(tvShow);
+  const [shows] = useState(tvShows);
   const [selectedShow, setSelectedShow] = useState();
   return (
     <>
       <header>
         <p>React TV</p>
-<ShowSelection 
-  shows={shows}
-  selectedShow={selectedShow}
-  setSelectedShow={setSelectedShow}
-/>
+        <ShowSelection
+          shows={shows}
+          selectedShow={selectedShow}
+          setSelectedShow={setSelectedShow}
+        />
       </header>
       <main>
-        <ShowDetails key={selectedShow?.name} shows={selectedShow} />
+        <ShowDetails key={selectedShow?.name} show={selectedShow} />
       </main>
     </>
   );
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,22 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({
+  shows,
+  selectedShow,
+  setSelectedShow,
+}) {
+  return (
+    <nav className="shows">
+      {shows.map((show) => (
+        <a
+          key={show.name}
+          onClick={() => setSelectedShow(show)}
+          className={"show" + (show === selectedShow ? " selected " : "")}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
